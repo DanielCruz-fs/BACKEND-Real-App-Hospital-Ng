84fd11f4cbba0506284ce32b3fb7283cd38d4d67
daniel cruz <dannyfsociety@gmail.com>
2019-02-19 23:00:36 -0400

    CRUD API USER :tada:

--- models/user.js
+++ models/user.js
@@ -1,12 +1,17 @@
 var mongoose = require('mongoose');
-
+var uniqueValidator = require('mongoose-unique-validator');
 var Schema = mongoose.Schema;
 
+var rolesAllowed = {
+  values: ['ADMIN_ROLE', 'USER_ROLE'],
+  message: '{VALUE} is not allowed'
+}
 var userSchema = new Schema({
   name: { type: String, required: [true, 'Name is required'] },
   email: { type: String, unique: true, required: [true, 'Email is required'] },
   password: { type: String, required: [true, 'Password is required'] },
   img: { type: String, required: false },
-  role: { type: String, required: true, default: 'USER_ROLE' },
+  role: { type: String, required: true, default: 'USER_ROLE', enum: rolesAllowed },
 });
+userSchema.plugin(uniqueValidator, { message: 'The Email must be unique' });
 module.exports = mongoose.model('User', userSchema);
--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,10 @@
 var express = require('express');
+var bcrypt = require('bcryptjs');
 var app = express();
 var User = require('../models/user');
-/**Obtain all users */
+// ==========================
+/** Obtain all users */
+// ==========================
 app.get('/', (request, response, next) => {
   User.find({}, 'name email img role').exec((err, users) => {
     if (err) {
@@ -18,21 +21,68 @@ app.get('/', (request, response, next) => {
     });
   });
 });
+// ============================
+/** Update user */
+// ============================
+app.put('/:id', (req, res) => {
+  var id = req.params.id;
+  var body = req.body;
 
-/**Create new user */
+  User.findById(id, (err, user) => {
+    if (err) {
+      return res.status(400).json({
+        ok: false,
+        message : 'Error finding user',
+        errors: err
+      });
+    }
+
+    if (!user) {
+      return res.status(400).json({
+        ok: false,
+        message : 'User with id: ' + id + 'no existe',
+        errors: { message: 'There is no user' }
+      });
+    }
+
+    user.name = body.name;
+    user.email = body.email;
+    user.role = body.role;
+
+    user.save((err, userUpdated) => {
+      if (err) {
+        return res.status(400).json({
+          ok: false,
+          message : 'Error in updating user',
+          errors: err
+        });
+      }
+      userUpdated.password = 'XD';
+
+      res.status(200).json({
+        ok: true,
+        user: userUpdated
+      });
+    });
+
+  });
+});
+// ============================
+/** Create new user */
+// ============================
 app.post('/', (req, res) => {
   var body = req.body;
 
   var user = new User({
     name: body.name,
     email: body.email,
-    password: body.password,
+    password: bcrypt.hashSync(body.password, 10),
     img: body.img,
     role: body.role
   })
   user.save((err, userCreated) => {
     if (err) {
-      return res.status(500).json({
+      return res.status(400).json({
         ok: false,
         message : 'Error creating user',
         errors: err
@@ -44,5 +94,31 @@ app.post('/', (req, res) => {
     });
   });
   
+});
+// ============================
+/** Delete user bi Id */
+// ============================
+app.delete('/:id', (req, res) => {
+  var id = req.params.id;
+  User.findByIdAndRemove(id, (err, userRemoved) => {
+    if (err) {
+      return res.status(400).json({
+        ok: false,
+        message : 'Error removing user',
+        errors: err
+      });
+    }
+    if (!userRemoved) {
+      return res.status(400).json({
+        ok: false,
+        message : 'User no longer exists',
+        errors: { message: 'No reference for the current id' }
+      });
+    }
+    res.status(201).json({
+      ok: true,
+      users: userRemoved
+    });
+  });
 });
 module.exports = app;
