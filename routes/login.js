bd9b0438e9b12fa2c785e3f2ded4ff46c9abfe59
daniel cruz <dannyfsociety@gmail.com>
2019-03-02 15:54:56 -0400

    Google Sogn In Back End Server :metal:

--- routes/login.js
+++ routes/login.js
@@ -4,6 +4,51 @@ var jwt = require('jsonwebtoken');
 var app = express();
 var User = require('../models/user');
 
+// Google verification TOKEN
+var CLIENT_ID = '761908812877-5muo95srnpdi8djjtla8940a349eo24f.apps.googleusercontent.com';
+const {OAuth2Client} = require('google-auth-library');
+const client = new OAuth2Client(CLIENT_ID);
+
+// ================================
+// Google Authentication
+// ================================
+async function verify(token) {
+  const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+      // Or, if multiple clients access the backend:
+      //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+  });
+  const payload = ticket.getPayload();
+  //const userid = payload['sub'];
+  // If request specified a G Suite domain:
+  //const domain = payload['hd'];
+  return {
+    name: payload.name,
+    email: payload.email,
+    img: payload.picture,
+    google: true
+  }
+}
+
+app.post('/google', async (req, res, next) => {
+  var token = req.body.token;
+
+  var googleUser = await verify(token).catch(e => {
+    return res.status(403).json({
+      ok: false,
+      message: 'Invalid Token'
+    });
+  });
+
+  res.status(200).json({
+    ok: true,
+    googleUser : googleUser
+  });
+});
+// ================================
+// Common Authentication
+// ================================
 app.post('/', (req, res) => {
   var body = req.body;
 
